b68925a01dd62a05abaf737a361a64d302eefa6c
sarahwangx <sarahwang@Sarahs-MacBook-Pro.local>
2017-01-24 01:30:21 -0800

    studio2

---
+++ studio2/script.js
@@ -0,0 +1,9 @@
+// p5 file
+
+// open the console window of any browser to see console.log() messages,
+// like print() messages in Processing
+
+//Sarah Wang | DES 157 Winter 2016 | Glenda Drew
+//
+
+console.log("Testing Console");
