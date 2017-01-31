e02360d76e2fe9df990a87c56d0e1110c1a38381
sarahwangx <sarahwang@Sarahs-MacBook-Pro.local>
2017-01-30 19:28:53 -0800

    s2

--- studio2/script.js
+++ studio2/script.js
@@ -6,4 +6,184 @@
 //Sarah Wang | DES 157 Winter 2016 | Glenda Drew
 //
 
-console.log("Testing Console");
+console.log('reading');
+
+//capture the submit event
+  document.f.onsubmit = processForm;
+
+  //define process function
+  function processForm() {
+
+    //store user noun1 in a variable
+    var userNoun1 = document.f.userNoun1.value;
+    //alert('Hi ' + userNoun);
+
+    //store user noun2 in a variable called userNoun2
+
+    var userNoun2 = document.f.userNoun2.value;
+
+    //comment out the alert message above
+
+    //call a new alert() to concatenate a message with userName and userColor
+
+    //capture the myMsg element
+    var myMsg = document.getElementById('myMsg');
+
+    //calling innerHTML to concatenate a message with userName and userColor
+
+    myMsg.innerHTML =
+    'My ' + userNoun1 + ' don/t, my ' + userNoun1 + ' don/t,<br>'
+    + 'My ' + userNoun1 + ' don/t, want none unless you got ' + userNoun2 + ', hun,<br>'
+
+
+    'Boy toy named Troy used to live in Detroit'
+
+    "Big dope dealer money, he was gettin' some coins"
+
+    'Was in shootouts with the law, but he live in a palace'
+
+    'Bought me Alexander McQueen, he was keeping me stylish'
+
+    'Now that/s real, real, real'
+
+    'Gun in my purse, bitch, I came dressed to kill'
+
+    'Who wanna go first? I had them pushing daffodils'
+
+    'I/m high as hell, I only took a half a pill'
+
+    'I/m on some dumb shit, by the way, what he say?'
+
+    'He can tell I ain/t missing no meals'
+
+    'Come through and fuck him in my automobile'
+
+    "Let him eat it with his grills and he tellin' me to chill"
+
+    'And he telling me it/s real, that he love my sex appeal'
+
+    "Say he don't like 'em boney, he want something he can grab"
+
+    'So I pulled up in the Jag, and I hit him with the jab like'
+
+    'Dun-d-d-dun-dun-d-d-dun-dun'
+
+
+    'My anaconda don/t, my anaconda don/t'
+
+    'My anaconda don/t want none unless you got buns, hun'
+
+
+    'Oh my gosh, look at her butt'
+
+    'Oh my gosh, look at her butt'
+
+    'Oh my gosh, look at her butt'
+
+    "(Look at her butt)"
+
+    'Look at, look at, look at'
+
+    'Look, at her butt'
+
+
+    'This dude named Michael used to ride motorcycles'
+
+    'Dick bigger than a tower, I ain/t talking about Eiffel/s'
+
+    'Real country-ass nigga, let me play with his rifle'
+
+    'Pussy put his ass to sleep, now he calling me NyQuil'
+
+    'Now that bang, bang, bang'
+
+    'I let him hit it cause he slang cocaine'
+
+    'He toss my salad like his name Romaine'
+
+    'And when we done, I make him buy me Balmain'
+
+    'I/m on some dumb shit, by the way, what he say?'
+
+    'He can tell I ain/t missing no meals'
+
+    'Come through and fuck him in my automobile'
+
+    'Let him eat it with his grills, and he telling me to chill'
+
+    'And he telling me it/s real, that he love my sex appeal'
+
+    "He say he don't like 'em boney, he want something he can grab"
+
+    'So I pulled up in the Jag, Mayweather with the jab like'
+
+    'Dun-d-d-dun-dun-d-d-dun-dun'
+
+
+    'My anaconda don/t, my anaconda don/t'
+
+    'My anaconda don/t want none unless you got buns, hun'
+
+
+    'Oh my gosh, look at her butt'
+
+    'Oh my gosh, look at her butt'
+
+    'Oh my gosh, look at her butt'
+
+    "(Look at her butt)"
+
+    'Look at, look at, look at'
+
+    'Look, at her butt<br>'
+
+
+    'Little in the middle but she got much back<br>'
+
+    'Little in the middle but she got much back<br>'
+
+    'Little in the middle but she got much back<br>'
+
+    "(Oh my God, look at her butt)<br>"
+
+
+    'My anaconda don/t, my anaconda don/t<br>'
+
+    'My anaconda don/t want none unless you got buns, hun<br>'
+
+    'My anaconda don/t, my anaconda don/t<br>'
+
+    'Don/t want none unless you got buns, hun<br>'
+
+
+    'Oh my gosh, look at her butt<br>'
+
+    'Oh my gosh, look at her butt<br>'
+
+    'Oh my gosh, look at her butt<br>'
+
+    "(Look at her butt)<br>"
+
+    'Look at, look at, look at<br>'
+
+    'Look, at her butt<br>'
+
+
+    'Yeah, he love this fat ass, ha haha!<br>'
+
+    'Yeah! This one is for my bitches with a fat ass in the fucking club<br>'
+
+    'I said, where my fat ass big bitches in the club?<br>'
+
+    'Fuck the skinny bitches! Fuck the skinny bitches in the club!<br>'
+
+    "I wanna see all the big fat ass bitches in the muthafuckin' club<br>"
+    'Fuck you if you skinny bitches, what?! Kyuh<br>'
+    'Ha ha, ha ha<br>'
+    'I got a big fat ass (ass, ass, ass)<br>'
+    'Come on!';
+
+     //prevent page from reloading
+    return false;
+
+  }
